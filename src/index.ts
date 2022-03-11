ab70488280db9d3c7dd12042bdcbf575387426c3
Matheus <madtheusss@hotmail.com>
2022-03-11 20:51:43 -0300

    Adicionando o typeScript e incrementando a forma de desenvolvimento

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Executando");
+import express, { Request, Response, NextFunction } from "express"; //Gerenciador de Rotas HTTP
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
+});
+
+app.listen(3000, ()=>{
+    console.log("Aplicação executando com sucesso na porta 3000");
+})

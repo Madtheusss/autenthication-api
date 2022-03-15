f43845dcbdb0ec96f0b52daea4bbbf68c38a7e75
Matheus <madtheusss@hotmail.com>
2022-03-15 11:47:07 -0300

    Refatorando a rota de status

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express"; //Gerenciador de Rotas HTTP
 import usersRoute from "./routes/users.route";
+import statusRoute from "./routes/status.route";
 
 const app = express();
 
@@ -9,10 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Configurações de Rotas
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'bar'});
-});
+app.use(statusRoute);
 
 //Inicialização do servidor
 app.listen(3000, () => {
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,5 @@
-import { Router, Request, Response, NextFunction, response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
-import { nextTick } from "process";
 
 const usersRoute = Router();
 

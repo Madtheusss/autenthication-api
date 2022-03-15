9ff3291a261a706764e1c53c4a990dfdf100d812
Matheus <madtheusss@hotmail.com>
2022-03-15 11:14:43 -0300

    Implementação do módulo PUT e DELETE

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response, NextFunction, response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { nextTick } from "process";
 
 const usersRoute = Router();
 
@@ -9,19 +10,34 @@ usersRoute.get('/users', (req:Request, res:Response, next:NextFunction) => {
     res.status(StatusCodes.OK).send(users);
 });
 
-// Criando um GET /users/:uuid
+// Buscando um usuario específico GET /users/:uuid
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) =>{
     const uuid = req.params.uuid;
     
     res.status(StatusCodes.OK).send({ uuid });
 });
 
+//Criando um POST /users
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
 
     console.log(req.body);
 
     res.status(StatusCodes.CREATED).send(newUser);
+});
+
+//Criando um PUT /users/:uuid
+usersRoute.put('/users/:uuid', (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send({ modifiedUser });
+}); 
+
+//Criando DELETE /users/:uuid
+usersRoute.delete('/users/:uuid', (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;

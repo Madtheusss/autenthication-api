462b8b60dead95b5d146dda5e2a5314ae61e5935
Matheus <madtheusss@hotmail.com>
2022-03-15 22:46:03 -0300

    Desenvolvendo inserção de usuários

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -18,12 +18,10 @@ usersRoute.get('/users/:uuid', async (req: Request<{ uuid: string }>, res: Respo
 });
 
 //Criando um POST /users
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-
-    console.log(req.body);
-
-    res.status(StatusCodes.CREATED).send(newUser);
+    const uuid = await userRepository.create(newUser);
+    res.status(StatusCodes.CREATED).send(uuid);
 });
 
 //Criando um PUT /users/:uuid

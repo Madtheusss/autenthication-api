5f3fc7b51796d409410d83567fc734b9425f7162
Matheus <madtheusss@hotmail.com>
2022-03-15 23:00:34 -0300

    Desenvolvendo update e o delete de usuarios

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -25,16 +25,21 @@ usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction
 });
 
 //Criando um PUT /users/:uuid
-usersRoute.put('/users/:uuid', (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
 
     modifiedUser.uuid = uuid;
-    res.status(StatusCodes.OK).send({ modifiedUser });
+
+    await userRepository.update(modifiedUser)
+
+    res.status(StatusCodes.OK).send();
 }); 
 
 //Criando DELETE /users/:uuid
-usersRoute.delete('/users/:uuid', (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request <{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    await userRepository.remove(uuid);
     res.sendStatus(StatusCodes.OK);
 });
 

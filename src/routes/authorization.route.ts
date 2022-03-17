4833d23fa42ba4440531c05d7e87b2e93b437df5
Matheus <madtheusss@hotmail.com>
2022-03-17 19:36:51 -0300

    Recebendo Token e validando Credenciais

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -17,6 +17,15 @@ authorizationRoute.post('/token', (req: Request, res: Response, next: NextFuncti
             throw new ForbiddenError("Tipo de autenticação invalida!")
         }
 
+        const tokenContent = Buffer.from(token,'base64').toString('utf-8');
+        const [username, password] = tokenContent.split(':');
+
+        if(!username || !password){
+            throw new ForbiddenError("Credenciais não preenchidas!");
+        }
+
+        console.log(username,password);
+
     } catch (error){
         next(error);
     }

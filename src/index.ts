8761817eba7023f35e368e705621567ebdbb3a05
Matheus <madtheusss@hotmail.com>
2022-03-17 19:29:10 -0300

    Recebendo Token e validando credenciais

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from "express"; //Gerenciado
 import usersRoute from "./routes/users.route";
 import statusRoute from "./routes/status.route";
 import errorHandler from "./middlewares/error-handler.middleware";
+import authorizationRoute from "./routes/authorization.route";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 //Configurações de Rotas
 app.use(usersRoute);
 app.use(statusRoute);
+app.use(authorizationRoute);
+
 
 //Configuração dos Habdlers de Erro
 app.use(errorHandler);
